ed99c70b0e487af06d6d65cbd5e4dd4cb6a96e0b
NamAnhk <anhbnth04496@fpt.edu.vn>
2024-11-15 11:16:53 +0700

    Update TestGit.java
    troll

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -19,7 +19,11 @@ public class TestGit {
         System.out.println("Chi done!!");
         System.out.println("huydz");
         system.out.println("Gkhanh");
-        
+
+        for(int i = 0; i<11; i++){
+    System.out.println(i+1);
+            
+        }
     }
     
 }
