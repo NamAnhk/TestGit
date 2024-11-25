dbee83d0bf17b2226c231651b136f02260c0a561
NamAnhk <anhbnth04496@fpt.edu.vn>
2024-11-25 08:36:58 +0700

    Update TestGit.java

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -14,26 +14,7 @@ public class TestGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Skibidi");
-        System.out.println("Chi done!!");
-        System.out.println("huydz");
-        system.out.println("Gkhanh");
-        systen.out.println("nhtkdzai");
-                system.out.println("noooooooooooooo");
-        switch(Giang){
-        leuleu;
-        }
-        for(int i = 0; i<11; i++){
-    System.out.println(i+1);
-          System.out.println("In ra cac so tu 1 den 10");
-         System.out.println("In ra cac so tu 1 den 10");
-            for(int i = 0; i<11;i++)
-                {
-                    System.out.println(i);            
-                }
-
-            
+        // TODO code application logic here       
           System.out.println("In ra cac so chan tu 1 den 10");
             for(int i = 0; i<11;i++)
                 {
