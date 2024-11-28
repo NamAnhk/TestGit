c03f40c22ad1f97c15a806a3a5acfa7ca6b7a526
nhtkdzai <156192297+nhtkdzai@users.noreply.github.com>
2024-11-28 22:10:09 +0700

    Update TestGit.java
    da upd commit cua huy

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -363,3 +363,22 @@ systen.out.println("chi commit lan 3 28/11");
         sql = "SELECT * FROM Tablets";
     }
     }
+
+  QLMayTinh ql = new QLMayTinh();
+    ArrayList<MayTinh> list = ql.getList();
+ArrayList<Integer> listadd = ql.getListadd();
+    void writeFile() {
+        String path = "output.txt";
+        ArrayList<MayTinh> list = ql.getList();
+        try {
+            FileWriter write = new FileWriter(path);
+            BufferedWriter buffwrite = new BufferedWriter(write);
+            for (MayTinh mayTinh : list) {
+                buffwrite.write(mayTinh.toString());
+                buffwrite.newLine();
+            }
+            buffwrite.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
