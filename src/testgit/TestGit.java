ffed6274cf83b39b722853a08c007338e2d6e43d
huypony <huypoly1127@gmail.com>
2024-12-05 20:51:45 +0700

    Update TestGit.java

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -17,3 +17,22 @@ ArrayList<Integer> listadd = ql.getListadd();
             System.out.println(e.getMessage());
         }
     }
+
+    QLMayTinh ql = new QLMayTinh();
+    ArrayList<MayTinh> list = ql.getList();
+ArrayList<Integer> listadd = ql.getListadd();
+    void writeFile() {
+        String path = "output.txt";
+        ArrayList<MayTinh> list = ql.getList();
+        try {
+            FileWriter write = new FileWriter(path);
+            BufferedWriter buffwrite = new BufferedWriter(write);
+            for (MayTinh mayTinh : list) {
+                buffwrite.write(mayTinh.toString());
+                buffwrite.newLine();
+            }
+            buffwrite.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
