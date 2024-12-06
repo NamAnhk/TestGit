da90639ba7023a0005e5e45955a0150eaa916303
Cheese1411 <chintktu00027@fpt.edu.vn>
2024-12-06 16:41:51 +0700

    Update TestGit.java

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -166,6 +166,6 @@ systen.out.println("chi commit lan 3 05/12");
 systen.out.println("chi commit lan 2 05/12");
 systen.out.println("chi commit lan 1 05/12");
 systen.out.println("chi commit lan 0 05/12");
-systen.out.println("chi commit lan 0 05/12");
-
+systen.out.println("chi commit lan 2 05/12");
+systen.out.println("chi commit lan 1 06/12");
     }
