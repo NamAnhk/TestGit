67d5c9e15de8b57396466c52e18bb824b7cda26a
Gkhanh0412 <giakhanhnguyen0401@gmail.com>
2024-12-09 08:08:19 +0700

    Gkhanh

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -190,5 +190,18 @@ private boolean isPrime(int num) {
     }
     return true;
 }
+    public void toggleCase(String input) {
+    StringBuilder result = new StringBuilder();
+    for (char c : input.toCharArray()) {
+        if (Character.isUpperCase(c)) {
+            result.append(Character.toLowerCase(c));
+        } else if (Character.isLowerCase(c)) {
+            result.append(Character.toUpperCase(c));
+        } else {
+            result.append(c);
+        }
+    }
+    System.out.println("Chuỗi sau khi chuyển đổi chữ hoa/thường: " + result);
+}
     
 }
